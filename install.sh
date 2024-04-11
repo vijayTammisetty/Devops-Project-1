ea29ade81b80ffc5e07f793984e991e320569cd1
vijayTammisetty <120720424+vijayTammisetty@users.noreply.github.com>
2024-04-11 15:40:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
 sudo yum update -y
 
 #---------------git install ---------------
 
 sudo yum install git -y
 
+
 #-------java dependency for jenkins------------
 
-sudo yum install java-11-amazon-corretto -y
+sudo dnf install java-11-amazon-corretto -y
 
 #------------jenkins install-------------
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -25,6 +25,8 @@ sudo systemctl start jenkins
 #cd bin
 #chmod +x startup.sh
 
+
+
 #---------------------------Maven install -------------
 sudo yum install maven -y
 
@@ -32,22 +34,17 @@ sudo yum install maven -y
 sudo curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin 
-
 # -----------------------------eksctl install--------------------------------
 sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
-
 #----------------------Trivy install---------------
 sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.48.3/trivy_0.48.3_Linux-64bit.rpm
-
 
 #----------------------sonarQube install-----------------------------------
 sudo yum -y install wget nfs-utils
 sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
 sudo yum -y install sonar
-
-
 #-----------------------JFROg-----------------------------
 sudo wget https://releases.jfrog.io/artifactory/artifactory-rpms/artifactory-rpms.repo -O jfrog-artifactory-rpms.repo;
 
@@ -57,13 +54,6 @@ sudo yum update && sudo yum install jfrog-artifactory-oss -y
 
 sudo systemctl start artifactory.service
 
-
-#-------------------terraform install--------------------------
-
-sudo wget https://releases.hashicorp.com/terraform/1.7.2/terraform_1.7.2_linux_amd64.zip
-sudo unzip terraform_1.7.2_linux_amd64.zip
-sudo mv terraform /usr/local/bin
-
 #------------------Docker install-------------
 #sudo amazon-linux-extras install docker #linux 2022
 sudo yum install docker -y #linux 2023
@@ -72,8 +62,6 @@ sudo usermod -aG docker jenkins
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
 sudo service docker start
-
-
 #------------------sonar install by using docker---------------
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 docker run -d --name tomcat -p 8089:8080 tomcat:lts-community
